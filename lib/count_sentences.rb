8cf7a0341437ef8810dc17c78b29b53bff72f5f0
Jonathan Block <blockjonathan29@gmail.com>
2021-02-12 11:51:26 -0500

    commit tree 1f6424d8c09c24c2f690576dc162a1c987a31ebc

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,33 @@
 require 'pry'
 
 class String
-
+   
   def sentence?
-    
+   if self.end_with?(".") == true 
+    return true
+   else
+    return false
+   end
   end
 
   def question?
-
+  if self.end_with?("?") == true 
+    return true
+   else
+    return false
+  end
   end
 
   def exclamation?
-
+    if self.end_with?("!") == true 
+      return true
+     else
+      return false
+    end
   end
 
   def count_sentences
-
+    self.split(/[!?.]/).reject{|c| c.empty?}.count
   end
+
 end
